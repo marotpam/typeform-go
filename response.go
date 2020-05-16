f34b04297f29be6215f23a3e01ea31f7b0f2def9
Marcel Otón Pàmies <marotpam@gmail.com>
2020-05-16 18:42:29 +0200

    Add response struct

---
+++ response.go
@@ -0,0 +1,58 @@
+package typeform
+
+import "time"
+
+type ResponseListParams struct {
+	PageSize            int        `query:"page_size,omitempty"`
+	Since               *time.Time `query:"since,omitempty"`
+	Until               *time.Time `query:"until,omitempty"`
+	After               *time.Time `query:"after,omitempty"`
+	Before              *time.Time `query:"before,omitempty"`
+	IncludedResponseIDs string     `json:"included_response_ids"`
+	Completed           *bool      `query:"completed,omitempty"`
+	Sort                string     `query:"sort,omitempty"`
+	Query               string     `query:"query,omitempty"`
+	Fields              string     `query:"fields,omitempty"`
+}
+
+type ResponseList struct {
+	TotalItems int        `json:"total_items"`
+	PageCount  int        `json:"page_count"`
+	Items      []Response `json:"items"`
+}
+
+type Response struct {
+	LandingID   string    `json:"landing_id"`
+	Token       string    `json:"token"`
+	ResponseID  string    `json:"response_id,omitempty"`
+	LandedAt    time.Time `json:"landed_at"`
+	SubmittedAt time.Time `json:"submitted_at"`
+	Metadata    struct {
+		UserAgent string `json:"user_agent"`
+		Platform  string `json:"platform"`
+		Referer   string `json:"referer"`
+		NetworkID string `json:"network_id"`
+		Browser   string `json:"browser"`
+	} `json:"metadata"`
+	Answers []struct {
+		Field struct {
+			ID   string `json:"id"`
+			Type string `json:"type"`
+			Ref  string `json:"ref"`
+		} `json:"field"`
+		Type    string `json:"type"`
+		Text    string `json:"text,omitempty"`
+		Boolean bool   `json:"boolean,omitempty"`
+		Email   string `json:"email,omitempty"`
+		Number  int    `json:"number,omitempty"`
+		Choices struct {
+			Labels []string `json:"labels"`
+		} `json:"choices,omitempty"`
+		Date   time.Time `json:"date,omitempty"`
+		Choice struct {
+			Label string `json:"label"`
+		} `json:"choice,omitempty"`
+	} `json:"answers"`
+	Hidden     map[string]string      `json:"hidden,omitempty"`
+	Calculated map[string]interface{} `json:"calculated"`
+}
